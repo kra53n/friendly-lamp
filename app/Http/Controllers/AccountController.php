0274b1a240716a6afe38d59a663bec0b30d5b584
kra53n <greasha46@gmail.com>
2025-01-24 18:50:35 +0300

    add `/accounts/{id}` api

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -28,9 +28,55 @@ class AccountController extends Controller
         return response()->json($account, 201);
     }
 
+    public function get_by_id($id) {
+        $accounts = Account::where('user_id','=', $id);
+        if ($accounts->count() > 0) {
+            return response()->json($accounts->get(), 200);
+        }
+        if (is_numeric($id)) {
+            return response()->json(sprintf('there is no user with id %d', $id), 404);
+        }
+        return response()->json('should be given integer', 404);
+    }
+
+    public function update_by_id(Request $request, $id) {
+        $validator = $this->validate_update_request($request);
+ 
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $validated = $validator->validated();
+        $account = Account::find($id);
+        if ($account == null) {
+            return response()->json(sprintf('there is no account with id %d', $id), 404);
+        }
+        $account->fill($validated);
+        $account->save();
+        return response()->json($account, 200);
+    }
+
+    public function delete_by_id($id) {
+        $account = Account::find($id);
+        $account->delete();
+        return response()->json($account, 200);
+    }
+
     public function validate_store_request(Request $request) {
         $validation_rulse = [
             'user_id' => 'required|integer|exists:users,id',
+            'balance' => 'required|numeric|min:0',
+        ];
+        $validation_err_msgs = [
+            'user_id' => 'The :attribute must exists',
+            'balance' => 'The :attribute muse bet at least :min',
+        ];
+        return Validator::make($request->all(), $validation_rulse, $validation_err_msgs);
+    }
+
+    public function validate_update_request(Request $request) {
+        $validation_rulse = [
+            'user_id' => 'integer|exists:users,id',
             'balance' => 'required|numeric|min:0',
         ];
         $validation_err_msgs = [
--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,8 @@ Route::get('/', function () {
 Route::prefix("accounts")->group(function () {
     Route::get('/', [AccountController::class, 'get_all_accounts']);
     Route::post('/', [AccountController::class, 'store']);
+    Route::get('{id}', [AccountController::class, 'get_by_id']);
+    Route::put('{id}', [AccountController::class, 'update_by_id']);
+    Route::patch('{id}', [AccountController::class, 'update_by_id']);
+    Route::delete('{id}', [AccountController::class, 'delete_by_id']);
 });
