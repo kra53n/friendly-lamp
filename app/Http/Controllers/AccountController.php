6de03c7564380e472a749853ff149aa78fbcc06f
kra53n <greasha46@gmail.com>
2025-01-24 18:01:14 +0300

    add `/accounts` api

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\Account;
+
+class AccountController extends Controller
+{
+    public function get_all_accounts(Request $request) {
+        return response()->json(Account::all());
+    }
+
+    public function store(Request $request) {
+        $validation_rulse = [
+            'user_id' => 'required|integer|exists:users,id',
+            'balance' => 'required|numeric|min:0',
+        ];
+        $validation_err_msgs = [
+            'user_id' => 'The :attribute must exists',
+            'balance' => 'The :attribute muse bet at least :min',
+        ];
+        $validator =  Validator::make($request->all(), $validation_rulse, $validation_err_msgs);
+ 
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $validated = $validator->validated();
+        $account = Account::create([
+            'user_id' => $validated['user_id'],
+            'balance' => $validated['balance'],
+        ]);
+        return response()->json($account, 201);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix("accounts")->group(function () {
+    Route::get('/', [AccountController::class, 'get_all_accounts']);
+    Route::post('/', [AccountController::class, 'store']);
 });
