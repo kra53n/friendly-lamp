4edd4acc74c671545a163e86ae66c46d96320b3a
kra53n <greasha46@gmail.com>
2025-01-25 23:41:49 +0300

    fix `TransactionService` usage in `TransactionController`

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,46 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TransactionService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 use App\Models\Transaction;
-use App\Services\TransactionService;
+// use App\Services\TransactionService;
 
 class TransactionController extends Controller
 {
+    protected TransactionService $transactionService;
+
+    public function __construct(
+        TransactionService $transactionService
+    ) {
+        $this->transactionService = $transactionService;
+    }
+
     public function get_all(Request $request) {
         return response()->json(Transaction::all());
     }
 
     public function store(Request $request) {
         $validator = $this->validate_store_request($request);
- 
+
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
 
         $validated = $validator->validated();
-        $validated = TransactionService::changeStatus($validated);
+        $validated = $this->transactionService->changeStatus($validated);
         $transaction = new Transaction;
         $transaction->fill($validated);
+        $transaction->save();
         return response()->json($transaction, 201);
     }
 
     public function get_by_id($id) {
+        if (!is_numeric($id)) {
+            return response()->json('should be given integer', 404);
+        }
         $transaction = Transaction::find($id);
         if ($transaction == null) {
             return response()->json(sprintf('there is no transaction with id %d', $id), 404);
-        }
-        if (is_numeric($id)) {
-            return response()->json('should be given integer', 404);
         }
         return response()->json($transaction, 200);
     }
 
     public function update_by_id(Request $request, $id) {
         $validator = $this->validate_update_request($request);
- 
+
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
@@ -68,23 +78,23 @@ class TransactionController extends Controller
             'to_account_id' => 'required|integer|exists:accounts,id|different:from_account_id',
             'amount' => 'required|numeric|min:0.01',
         ];
-        
+
         $validation_err_msgs = [
             'from_account_id.required' => 'The :attribute field is required.',
             'from_account_id.integer' => 'The :attribute must be an integer.',
             'from_account_id.exists' => 'The account with ID :attribute does not exist.',
             'from_account_id.different' => 'The from_account_id and to_account_id fields must be different.',
-            
+
             'to_account_id.required' => 'The :attribute field is required.',
             'to_account_id.integer' => 'The :attribute must be an integer.',
             'to_account_id.exists' => 'The account with ID :attribute does not exist.',
             'to_account_id.different' => 'The from_account_id and to_account_id fields must be different.',
-        
+
             'amount.required' => 'The :attribute field is required.',
             'amount.numeric' => 'The :attribute must be a number.',
             'amount.min' => 'The amount must be greater than zero.',
         ];
-        
+
         return Validator::make($request->all(), $validation_rules, $validation_err_msgs);
     }
 
--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -1,16 +1,16 @@
-<?
+<?php
 
 namespace App\Services;
 
 use App\Models\Account;
 
 class TransactionService {
-    public static function changeStatus(array $properties): array {
+    public function changeStatus(array $properties): array {
         $status = 'pending';
         if (array_key_exists('status', $properties)) {
             $status = $properties['status'];
         }
-        $from_account_balance = Account::find($properties['from_account_id']);
+        $from_account_balance = Account::find($properties['from_account_id'])['balance'];
         $amount = $properties['amount'];
         if ($amount > $from_account_balance) {
             $status = 'failed';
