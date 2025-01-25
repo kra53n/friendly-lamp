4f08d316f29b15d46a4602cebf4a1a9128934423
kra53n <greasha46@gmail.com>
2025-01-25 21:36:23 +0300

    add tests for `/accounts`

---
+++ tests/Feature/AccountTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\Models\Account;
+use App\Models\User;
+
+class AccountTest extends TestCase
+{
+    public function test_get_by_id(): void
+    {
+        $account = Account::factory()->create();
+        $account->save();
+        $response = $this->get(sprintf('/accounts/%d', $account->id));
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'id' => $account->id,
+            'user_id' => $account->user_id,
+            'balance' => $account->balance,
+        ]);
+    }
+
+    public function test_store_with_good_data(): void
+    {
+        $user = User::factory()->create();
+        $data = [
+            'user_id' => $user->id,
+            'balance' => 100,
+        ];
+        $response = $this->postJson('/accounts', $data);
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('accounts', $data);
+    }
+
+    public function test_store_with_bad_balance(): void
+    {
+        $user = User::factory()->create();
+        $data = [
+            'user_id' => $user->id,
+            'balance' => -1,
+        ];
+        $response = $this->postJson('/accounts', $data);
+        $response->assertStatus(400);
+    }
+
+    public function test_update_by_id(): void
+    {
+        $account = Account::factory()->create();
+        $account->save();
+        $balance_before = $account['balance'];
+        $data = [
+            'balance' => 3333,
+        ];
+        $response = $this->putJson(sprintf('/accounts/%d', $account->id), $data);
+        $response->assertStatus(200);
+        $account->refresh();
+        $this->assertNotEquals($balance_before, $account['balance']);
+    }
+}
