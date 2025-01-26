6adfdc930b95fa19f50aeedf187b0abcbf27b5ed
kra53n <greasha46@gmail.com>
2025-01-26 22:08:00 +0300

    fix problem with AccountService (5aea5c75f6f32e5aa2329e5f3d5f1ec24f76cbd3)

--- app/Console/Commands/TransactionsProcess.php
+++ app/Console/Commands/TransactionsProcess.php
@@ -4,8 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-use App\Models\Account;
 use App\Models\Transaction;
+use App\Services\AccountService;
 
 class TransactionsProcess extends Command
 {
@@ -26,7 +26,7 @@ class TransactionsProcess extends Command
     public function handle()
     {
         $transactions = Transaction::where('status', 'pending')->get();
-        $accounts = self::get_accounts_by_transactions($transactions);
+        $accounts = AccountService::get_accounts_by_transactions($transactions);
         $failed_transactions = 0;
         foreach ($transactions as $transaction) {
             self::process_transaction($transaction, $accounts);
@@ -56,28 +56,5 @@ class TransactionsProcess extends Command
         }
         $transaction['status'] = 'failed';
         $transaction->save();
-    }
-
-    // PROBLEM(kra53n): commit: 5aea5c75f6f32e5aa2329e5f3d5f1ec24f76cbd3
-    /**
-     * @param \Illuminate\Support\Collection<int, Transaction>
-     * @return "dictionary" where key is account_id and value is account model
-     */
-    public static function get_accounts_by_transactions($transactions): array {
-        $accounts = [];
-        foreach ($transactions as $transaction) {
-            $accounts = self::add_account_to_accounts_by_id($transaction['from_account_id'], $accounts);
-            $accounts = self::add_account_to_accounts_by_id($transaction['to_account_id'], $accounts);
-        }
-        return $accounts;
-    }
-
-    private static function add_account_to_accounts_by_id(int $account_id, $accounts): array {
-            if (array_key_exists($account_id, $accounts)) {
-                return $accounts;
-            }
-            $account = Account::find($account_id);
-            $accounts[$account_id] = $account;
-            return $accounts;
     }
 }
